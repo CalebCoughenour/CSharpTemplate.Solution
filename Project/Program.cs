8beb537389603363b8eab0b71b9516ea7414d27d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-10 16:12:37 -0700

    add MVC file structure with style sheet included

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,9 +1,20 @@
-using Project.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-public class Program
+namespace NamespaceOfProject
 {
-  public static void Main()
+  public class Program
   {
-    
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
   }
 }
